4bad8d1b37c4b25730d8dcc13ce8249bec0aee66
Michael <htkim298@gmail.com>
2020-03-19 19:07:44 +0900

    [기능] CSS APP 추가
    css 파일 생성시 오류 발생으로 인한 파일 변환

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,14 @@
 // App.js
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import styles from './App.css';
+// require("./App.css");
 
 class App extends Component {
 	render() {
     	return (
-        <div>
-          <button>버튼</button>
+        <div className='gray-background'>
+          연습입니다
         </div>
       );
     }
